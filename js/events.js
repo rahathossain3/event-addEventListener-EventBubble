3b8f770551a78bcedf57e2e6362f874283de3a9e
rahathossain3 <rahathossain210@gmail.com>
2022-02-09 09:27:30 +0600

    ok

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function makeRed() {
 
 
 // option 3
-//handle blue button click by setting function name
+//handle blue button click by setting function name------------------------------------
 
 const blueButton = document.getElementById('make-blue-button');
 // console.log(blueButton);
@@ -14,3 +14,34 @@ blueButton.onclick = makeBlue;              //dont call function... other wish f
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+
+// handle  button click by anonymous function --------------------------------------------
+
+const greenBUtton = document.getElementById('make-green-button');
+// console.log(greenBUtton);
+//Anonymous Function in ---- নাম বিহিন function
+greenBUtton.onclick = function () {
+    document.body.style.backgroundColor = 'green';
+}
+
+// handle by using add eventLisenter--------------------------------------------------------
+
+const goldenButton = document.getElementById('make-golenrod');
+goldenButton.addEventListener('click', makeGoldenRod);
+
+function makeGoldenRod() {
+    document.body.style.backgroundColor = 'goldenrod';
+}
+
+// add Event lisener -----------------------------------------------------------------------
+const hotPinkButton = document.getElementById('make-hot-pink');
+hotPinkButton.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'hotpink';
+})
+
+//direct short cart--------------------------------------------------------------------------
+
+document.getElementById('light-blue').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'lightblue';
+})
